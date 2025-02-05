3cac1008c73b683e87470a7b4b9bf06060631f92
Manasa Poloju <polojumanasa9@gmail.com>
2025-02-05 21:05:48 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,79 @@
+#print a number
+print(2)
+
+#print multiple numbers
+print(1,3,5,7,9)
+
+#add+ #sub- #mul* #div/ #exp** #modulus% #floor division//
+a=6
+b=8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+print(a//b)
+
+#integer - A whole number (not a fractional number) that can be positive, negative or zero.
+print(type(9))
+
+#float - Represents real numbers with decimal points.
+print(type(6.5))
+
+#variable - Variable is a name that stores value in memory.
+int1=24682468
+int2=13571357
+print(int1+int2)
+print(int1-int2)
+print(int1*int2)
+print(int1/int2)
+
+#string - String is a sequence of characters enclosed in single, double or triple quotes.
+str1="Do not make excuses"
+print('Do not make excuses')
+print(type('Do not make excuses'))           
+print(len('Do not make excuses'))            
+str1="Do not make excuses"                  
+print(str1)
+str1="               "                    
+print(len(str1))                            
+
+#indexing - access a specific element in a sequence or gives a specific element from str
+str2="Hope your doing well"
+print(str2[5])
+print(str2[10])
+print(str2[0])
+print(str2[17])
+print(len(str2))
+print(str2[-9])
+print(str2[-18])
+
+#slicing
+str3="Chicago is different from Boston"
+print(str3[0:8])
+print(str3[5:20])
+print(str3[:18])
+print(str3[:])
+print(str3[9:10000])
+print(str3[-10:-20])
+print("check", str3[-5: -16: -2])
+
+#step
+str4="How was your weekend"
+print(str4[0: 17: 2])    
+print(str4[0: 15: 3])    
+print(str4[-3: 12: -3])
+
+
+
+
+
+
+
+
+
+
+
+
+
